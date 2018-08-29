471df0c6a4f0207286c2cfeba82cdbcce9c63c2f
ShoeShine9 <Jarednknott@gmail.com>
2018-08-29 15:32:27 -0600

    Add project files.

---
+++ env_jnksite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Jared.Knott\source\repos\WebDev\JnkSite\env_jnksite\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
